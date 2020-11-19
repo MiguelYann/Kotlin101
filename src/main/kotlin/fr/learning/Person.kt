1d67fdaae07c6c44ce4cd7d18045dbebbd1fa623
miguel <tsambamiguelito@gmail.com>
2020-11-19 22:34:22 +0100

    Initial commit

---
+++ src/main/kotlin/fr/learning/Person.kt
@@ -0,0 +1,8 @@
+package fr.learning
+
+class Person(var Name: String) {
+
+    fun walk() {
+        println("$Name can walk only")
+    }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,7 @@
+import fr.learning.Person
+
+fun main(args: Array<String>) {
+
+    val boy = Person("Miguel")
+    boy.walk();
+}
