ae832e04b87f2040624195ded6d169c11cd7cfc5
miguel <tsambamiguelito@gmail.com>
2020-11-22 02:05:39 +0100

    Loops, list and maps

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,8 @@
 import fr.learning.Person
+import kotlin.collections.HashMap
 
 fun main(args: Array<String>) {
+
     val boy = Person("Miguel", "3")
     val girl = Person("Miguel", "2")
     val chief = Person("Miguel", "1")
@@ -22,26 +24,62 @@ fun main(args: Array<String>) {
     carOne.Wheels = 4
 
     // Mark as nullable
-    val carTwo: Car? = null
+    val car: Car? = Car()
 
-    //Safe operator
-    carTwo?.Wheels = null
+    //Then Safe operator does all 🚔
+    car?.Wheels = 8
 
     //If as an Expression
-    val message = if (carOne.Wheels == carTwo?.Wheels) {
-        "We have 2 car"
+    val age = 25
+    val message = if (age > 18) {
+        "We are major"
     } else {
-        "Not cars"
+        "Not yet major"
     }
     println(message)
 
     //Try statement cool
+    val defaultValue = 0
     val value = try {
-        Integer.parseInt(message);
+        Integer.parseInt("You must throw exception");
     } catch (exception: NumberFormatException) {
-        0
+        defaultValue
     }
-    print(value)
+    println(value)
+
+    //Ranges
+    val rangeOf = 1..10
+    for (i in rangeOf) {
+        println("value $i")
+    }
+    for (i in 1 until  10 step 2) {
+        println("value $i")
+    }
+
+    //Define easily list of string
+    val programmingLanguages = listOf("Java","Kotlin","JavaScript", "Dart")
+
+    // For access index of a list, 👋🏽 For loop
+    for ((index, language) in programmingLanguages.withIndex()) {
+        println("Language $language stay at $index position")
+    }
+
+    //Loop on that list
+    for(language in programmingLanguages) {
+        println(language)
+    }
+
+
+    // Define map
+    val heightOfPlayers = HashMap<Double,String>()
+    heightOfPlayers[1.91] = "Curry"
+    heightOfPlayers[2.05] = "Giannis"
+    heightOfPlayers[2.02] = "Kevin D"
+
+    // Loop on that map finish Map.Entries unless you use Stream
+    for((height, player) in heightOfPlayers) {
+        println("Player $player has $height")
+    }
 
 }
 
