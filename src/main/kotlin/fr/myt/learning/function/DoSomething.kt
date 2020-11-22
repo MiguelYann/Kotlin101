2370dc8c95ec4be03c229736ae8593ddd1fc9261
miguel <tsambamiguelito@gmail.com>
2020-11-22 23:17:42 +0100

    infix functions

--- src/main/kotlin/fr/myt/learning/function/DoSomething.kt
+++ src/main/kotlin/fr/myt/learning/function/DoSomething.kt
@@ -12,12 +12,21 @@ fun discoverLanguage(language: String = "Java") {
 // In Java code, call me even if I know you don't like it
 }
 
-fun main(args: Array<String>) {
-    val text = "Boy"
-    print(text.makePluralOf())
-}
+
 
 //Extend functions
 fun String.makePluralOf(): String {
     return "${this}s";
+}
+
+fun main(args: Array<String>) {
+    val firstName = "Miguel"
+    val lastName = "Yann"
+ val fullName = firstName append lastName
+    print(fullName)
+}
+
+//INFIX function
+infix fun String.append(word: String): String {
+    return "$this $word"
 }
